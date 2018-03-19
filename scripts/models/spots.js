7e28e88a6cc948e8cbed55756c5c64f16b86db50
Steele Walston <steele.walston@gmail.com>
2018-03-19 16:52:20 -0700

    added some files and started spots.js

--- scripts/models/spots.js
+++ scripts/models/spots.js
@@ -1,6 +1,26 @@
 'use strict';
 
-// (function(module) {
+(function(module) {
+    
+    const template = Handlebars.compile($('#spot-list-template').html());
 
+    function Spot(data) {
+        Object.keys(data).map(key => this[key] = data[key]);
+    }
 
-// })(window.module);
+    Spot.prototype.toHtml = function() {
+        return template(this);
+    };
+
+    Spot.all = [];
+
+    Spot.fetchAll = () => {
+        return $.getJSON(`${API_URL}/spots`)
+            .then(data => {
+                Spot.all = data.map(each => new Spot(each));
+            });
+    };
+
+    module.Spot = Spot;
+
+})(window.module);
