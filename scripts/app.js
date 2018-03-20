1640ddab8991a0ce4be64e761e532cd2f64f8fcf
Keli Hansen <keli.p.hansen@gmail.com>
2018-03-20 11:04:37 -0700

    add view reset

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,19 @@
 'use strict';
 
 (function(module) {
+
+    const resetView = () => {
+        $('.view').hide();
+    };
+
     const Spot = module.Spot;
     const spotView = module.spotView;
-
+    
+    page('*', (ctx, next) => {
+        resetView();
+        next();
+    });
+    
     page('/', () => Spot.fetchAll().then(spotView.initListView));
 
     page({ hashbang: true });
