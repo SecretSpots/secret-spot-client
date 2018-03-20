3ecaa7cfab267b5258a3966381b8d37267b650da
Keli Hansen <keli.p.hansen@gmail.com>
2018-03-20 11:31:37 -0700

    add user model

--- scripts/app.js
+++ scripts/app.js
@@ -7,15 +7,18 @@
     };
 
     const Spot = module.Spot;
+    const User = module.User;
     const spotView = module.spotView;
     
     page('*', (ctx, next) => {
         resetView();
         next();
     });
-    
+
     page('/', () => Spot.fetchAll().then(spotView.initListView));
 
     page({ hashbang: true });
+
+    User.tryToken();
 
 })(window.module);
