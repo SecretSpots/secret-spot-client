57af35c3c46e61b66b387693b3bcd58c67caf241
Keli Hansen <keli.p.hansen@gmail.com>
2018-03-20 11:38:56 -0700

    add sign-up view

--- scripts/views/login-view.js
+++ scripts/views/login-view.js
@@ -1,6 +1,28 @@
 'use strict';
 
-// (function(module) {
+(function(module) {
+
+    const User = module.User;
+
+    const loginView = {};
+
+    let method = '';
+
+    loginView.initSignup = () => {
+        if(User.current) {
+            $('#auth-form').hide();
+            $('#logged-in').show();
+        }
+        else {
+            method = 'signup';
+            $('#auth-type').attr('href', '/auth/signin').text('Have an account? Sign in.');
+            $('#auth-form').off('submit').on('submit', handleSubmit);
+            $('#logged-in').hide();
+        }
+        $('#auth-view').show();
+    };
+
+    
 
 
-// })(window.module);
+})(window.module);
