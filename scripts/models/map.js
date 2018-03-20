6373ea2ee7cd825e56321c7187d3669b4757725d
Jen Lipton <jkl.lipton@gmail.com>
2018-03-20 14:36:07 -0700

    map additions

--- scripts/models/map.js
+++ scripts/models/map.js
@@ -2,12 +2,24 @@
 
 (function(module) {
 
+    const Location = module.Location;
+
     function Map(data) {
         Object.keys(data).map(key => this[key] = data[key]);
     }
 
-    Map.all = [];
-    const locations = []
+    Map.allMarkers = [];
+
+    Map.fetchAll = function() {
+        $.getJSON(`${API_URL}/spots`) //eslint-disable-line
+            .then(data => {
+                Location.all = data.map(each => new Location(each));
+            });
+    };
+
+    Map.makeMarkers = function(data) {
+        
+    };
 
     
     module.Map = Map;
